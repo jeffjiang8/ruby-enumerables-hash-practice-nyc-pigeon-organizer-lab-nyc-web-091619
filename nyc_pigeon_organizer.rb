20b99f30868e58616267c0e6f0a16b63985fa732
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-06 15:56:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_names = {}
+  data.each do |key, value|
+    value.each do |new_names, names|
+      names.each do |name|
+        if !pigeon_names[name]
+          pigeon_names[name] = {}
+        end 
+        if !pigeon_names[name][key]
+          pigeon_names[name][key] = []
+          
 end
