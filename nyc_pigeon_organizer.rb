7f79961feafda437dce88ee839cf8f5d99f9abb1
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-06 15:58:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_names = {}
+  data.each do |key, value|
+    value.each do |new_names, names|
+      names.each do |name|
+        if !pigeon_names[name]
+          pigeon_names[name] = {}
+        end 
+        if !pigeon_names[name][key]
+          pigeon_names[name][key] = []
+        end
+        pigeon_names[name][key].push(new_names.to_s)
+      end
+    end
+  end
+  pigeon_names
 end
